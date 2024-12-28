1f484e9798e65c0fc938bca090a06a991d0dd10b
abhishek17551 <ababhishek113@gmail.com>
2024-12-29 00:08:08 +0530

    created Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
+import Navbar from "./components/Navbar"
 
 function App() {  
  return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Om Tum Gajananaya Namah _/\_
-      </h1>
+      <Navbar/>
     </>
   )
 }
