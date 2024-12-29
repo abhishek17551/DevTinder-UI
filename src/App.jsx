26b192fc114f45b99810f7b7dbd778785ffbb0cc
abhishek17551 <ababhishek113@gmail.com>
2024-12-29 20:05:14 +0530

    defined basic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import Navbar from "./components/Navbar"
+
+
+import { BrowserRouter, Route, Routes } from "react-router"
+import Body from "./components/Body"
 
 function App() {  
  return (
     <>
-      <Navbar/>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body/>}>
+            <Route path="/" element={<div>Feed page</div>}/>
+            <Route path="/login" element={<div>Login page</div>}/>
+            <Route path="/connections" element={<div>Connections page</div>}/>
+            <Route path="/profile" element={<div>Profile page</div>}></Route>
+          </Route>
+          
+        </Routes>
+      </BrowserRouter>
+      
     </>
   )
 }
