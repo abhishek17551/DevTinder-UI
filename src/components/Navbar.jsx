ef3cc60e5711fa8dca553ac7cbe649ea8a83a1ad
abhishek17551 <ababhishek113@gmail.com>
2024-12-29 23:26:56 +0530

    updated Navbar with logged in  user avatar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Navbar = () => {
+  const user = useSelector(store => store.user)
   return (
     <div>
       <div className="navbar bg-base-300">
@@ -9,13 +11,18 @@ const Navbar = () => {
         </div>
         <div className="flex-none gap-2">
           <div className="dropdown dropdown-end mr-5">
-            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-              <div className="w-10 rounded-full">
-                <img
-                        alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-              </div>
-            </div>
+            {
+              user && (
+                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                  <div className="w-10 rounded-full">
+                  <img
+                    alt="User Avatar"
+                    src={user.photoUrl} />
+                  </div>
+                </div>
+              )
+            }
+
             <ul
               tabIndex={0}
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
