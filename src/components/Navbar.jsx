a034e337f9a83a737b722b0b8cd820c58773376e
abhishek17551 <ababhishek113@gmail.com>
2024-12-30 12:16:00 +0530

    modified to sustain user after refresh

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   const user = useSelector(store => store.user)
@@ -8,7 +9,7 @@ const Navbar = () => {
     <div>
       <div className="navbar bg-base-300">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">DevTinder</a>
+          <Link to="/" className="btn btn-ghost text-xl">DevTinder</Link>
         </div>
         <div className="flex-none gap-2">
           <div className="dropdown dropdown-end mr-5 flex ">
@@ -29,10 +30,10 @@ const Navbar = () => {
               tabIndex={0}
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
               <li>
-                <a className="justify-between">
+                <Link to='/profile' className="justify-between">
                   Profile
                   <span className="badge">New</span>
-                </a>
+                </Link>
               </li>
               <li><a>Settings</a></li>
               <li><a>Logout</a></li>
